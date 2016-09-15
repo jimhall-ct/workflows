769fd1bf366beee201b0a5fbe8b994b35eee79e1
Jim Hall <jim.hall@solarmedia.com>
2016-09-15 15:02:03 -0400

    gulp default task setup

--- gulpfile.js
+++ gulpfile.js
@@ -1,23 +1,29 @@
+// Setup Plugins
 var gulp = require('gulp'),
     webpack = require('gulp-webpack'),
+    compass = require('gulp-compass'),
     coffee = require('gulp-coffee');
 
-var coffeeSources = ['components/coffee/tagline.coffee'],
-    coffeeDestination = 'components/scripts';
+// Define source files for tasks
+var coffeeSources = ['components/coffee/tagline.coffee'];
 
 var jsSources = ['components/scripts/pixgrid.js',
                  'components/scripts/rclick.js',
                  'components/scripts/template.js',
                  'components/scripts/tagline.js'];
 
+var sassSources = ['components/sass/style.scss'];
+
+// Process Coffee Script files
 gulp.task('coffee', function() {
   gulp.src(coffeeSources)
-    .pipe(coffee({bare: true}).on('error', function() {
-        console.log("Error in CoffeeScript")
+    .pipe(coffee({bare: true}).on('error', function(error) {
+        console.log(error);
       }))
-    .pipe(gulp.dest(coffeeDestination))
+    .pipe(gulp.dest('components/scripts'))
 });
 
+// Process JavaScript files
 gulp.task('js', function() {
   gulp.src(jsSources)
     .pipe(webpack({
@@ -27,3 +33,25 @@ gulp.task('js', function() {
     }))
     .pipe(gulp.dest('builds/development/js'))
 });
+
+// Process SASS files
+gulp.task('compass', function() {
+  gulp.src('components/sass/style.scss')
+    .pipe(compass({
+      sass: 'components/sass',
+      css: 'builds/development/css',
+      image: 'builds/development/images',
+      environment: 'development',
+      comments: true,
+      style: 'expanded'
+    }))
+      .on('error', function(error) {
+        console.log(error);
+      })
+    // gulp dest doesn't seem to be necessary since compass
+    // outputs to css property in config settings above
+    //.pipe(gulp.dest('builds/development/css'))
+});
+
+// Setup default tasks ($> gulp) to run when launching gulp without arguments
+gulp.task('default', ['coffee', 'js', 'compass']);
