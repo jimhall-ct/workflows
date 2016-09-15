8c227c1f0a3e8b784c758c7295f32091267cfdfd
Jim Hall <jim.hall@solarmedia.com>
2016-09-15 08:57:59 -0400

    Add Webpack to configuration

--- gulpfile.js
+++ gulpfile.js
@@ -1,12 +1,29 @@
 var gulp = require('gulp'),
-    gutil = require('gulp-util'),
+    webpack = require('gulp-webpack'),
     coffee = require('gulp-coffee');
 
-var coffeeSources = ['builds/components/coffee/tagline.coffee'],
-    coffeeDestination = 'builds/components/scripts';
+var coffeeSources = ['components/coffee/tagline.coffee'],
+    coffeeDestination = 'components/scripts';
+
+var jsSources = ['components/scripts/pixgrid.js',
+                 'components/scripts/rclick.js',
+                 'components/scripts/template.js',
+                 'components/scripts/tagline.js'];
 
 gulp.task('coffee', function() {
   gulp.src(coffeeSources)
-    .pipe(coffee({bare: true}).on('error', gutil.log))
+    .pipe(coffee({bare: true}).on('error', function() {
+        console.log("Error in CoffeeScript")
+      }))
     .pipe(gulp.dest(coffeeDestination))
+});
+
+gulp.task('js', function() {
+  gulp.src(jsSources)
+    .pipe(webpack({
+      output: {
+        filename: 'bundle.js'
+      }
+    }))
+    .pipe(gulp.dest('builds/development/js'))
 });
