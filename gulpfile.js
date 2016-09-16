7995839980cdf850067c45f7fe0b907cdca11fa0
Jim Hall <jim.hall@solarmedia.com>
2016-09-16 12:14:50 -0400

    Add gulp-if and gulp-uglify

--- gulpfile.js
+++ gulpfile.js
@@ -1,18 +1,34 @@
 // Setup Plugins
 var gulp = require('gulp'),
-    webpack = require('gulp-webpack'),
+    util = require('gulp-util'),
+    gulpif = require('gulp-if'),
+    concat = require('gulp-concat'),
+    browserify = require('gulp-browserify'),
+    uglify = require('gulp-uglify'),
     compass = require('gulp-compass'),
-    coffee = require('gulp-coffee');
+    coffee = require('gulp-coffee'),
+    webserver = require('gulp-connect');
+
+var build,
+    outputDir,
+    sassStyle;
 
 // Define source files for tasks
+var htmlSources = ['builds/development/*.html'];
+
 var coffeeSources = ['components/coffee/tagline.coffee'];
 
-var jsSources = ['components/scripts/pixgrid.js',
-                 'components/scripts/rclick.js',
-                 'components/scripts/template.js',
-                 'components/scripts/tagline.js'];
+var jsSources = ['components/scripts/rclick.js',
+                 'components/scripts/pixgrid.js',
+                 'components/scripts/tagline.js',
+                 'components/scripts/template.js'
+                 ];
+
+var jsonSources = ['builds/development/js/*.json'];
 
 var sassSources = ['components/sass/style.scss'];
+
+var imageSources = ['builds/development/images/**/*'];
 
 // Process Coffee Script files
 gulp.task('coffee', function() {
@@ -26,12 +42,12 @@ gulp.task('coffee', function() {
 // Process JavaScript files
 gulp.task('js', function() {
   gulp.src(jsSources)
-    .pipe(webpack({
-      output: {
-        filename: 'bundle.js'
-      }
-    }))
-    .pipe(gulp.dest('builds/development/js'))
+    .pipe(concat('script.js'))
+    .pipe(browserify())
+    .pipe(gulpif(build === 'production', uglify()))
+    .pipe(gulp.dest(outputDir + 'js'))
+    // Reload web page when js files are modified
+    .pipe(webserver.reload())
 });
 
 // Process SASS files
@@ -39,11 +55,10 @@ gulp.task('compass', function() {
   gulp.src('components/sass/style.scss')
     .pipe(compass({
       sass: 'components/sass',
-      css: 'builds/development/css',
-      image: 'builds/development/images',
-      environment: 'development',
+      css: outputDir + 'css',
+      image: outputDir + 'images',
       comments: true,
-      style: 'expanded'
+      style: sassStyle
     }))
       .on('error', function(error) {
         console.log(error);
@@ -51,6 +66,21 @@ gulp.task('compass', function() {
     // gulp.dest doesn't seem to be necessary since compass
     // outputs to css property in config settings above
     //.pipe(gulp.dest('builds/development/css'))
+
+    // Reload web page when sass files are modified
+    .pipe(webserver.reload())
+});
+
+// Used by Watch task for reloading modifications
+gulp.task('html', function() {
+  gulp.src(htmlSources)
+  .pipe(webserver.reload())
+});
+
+// Used by Watch task for reloading modifications
+gulp.task('json', function() {
+  gulp.src(jsonSources)
+  .pipe(webserver.reload())
 });
 
 // Watch Task to update files when there is a change
@@ -58,7 +88,47 @@ gulp.task('watch', function() {
   gulp.watch(coffeeSources, ['coffee']);
   gulp.watch(jsSources, ['js']);
   gulp.watch('components/sass/*.scss', ['compass']);
+  gulp.watch(htmlSources, ['html']);
+  gulp.watch(jsonSources, ['json']);
+});
+
+gulp.task('webserver', function() {
+  webserver.server({
+    root: outputDir,
+    livereload: true
+  });
+});
+
+// Task for copying dev files to prod directories
+gulp.task('prodFiles', function() {
+  console.log("Copying files from development to production...")
+  // Copy HTML files to production directory
+  gulp.src(htmlSources)
+  .pipe(gulp.dest('builds/production'))
+  // Copy Image files to production directory
+  gulp.src(imageSources)
+  .pipe(gulp.dest('builds/production/images'))
+  // Copy JSON files to production directory
+  gulp.src(jsonSources)
+  .pipe(gulp.dest('builds/production/js'))
 });
 
 // Setup default tasks ($> gulp) to run when launching gulp without arguments
-gulp.task('default', ['coffee', 'js', 'compass']);
+gulp.task('default', ['developmentSetup', 'html', 'json', 'coffee', 'js', 'compass', 'webserver', 'watch']);
+
+gulp.task('developmentSetup', function() {
+  build = 'development';
+  outputDir = 'builds/development/';
+  sassStyle = 'expanded';
+  util.log(util.colors.white.bold.bgGreen("DEVELOPMENT CODE"));
+});
+
+gulp.task('productionSetup', function() {
+  build = 'production';
+  outputDir = 'builds/production/';
+  sassStyle = 'compressed';
+  util.log(util.colors.white.bold.bgRed("PRODUCTION CODE - DO NOT EDIT FILES"));
+});
+
+gulp.task('production', ['productionSetup', 'prodFiles', 'coffee', 'js', 'compass', 'webserver']);
+gulp.task('development', ['default']);
